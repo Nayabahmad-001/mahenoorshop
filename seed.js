19951c68ff8da87d3da9f4c905c1f59f9884f9a9
Nayabahmad-001 <nayab.ahmad7987@gmail.com>
2026-06-25 16:52:23 +0530

    latest commit

--- seed.js
+++ seed.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const Product = require('./models/Product');
